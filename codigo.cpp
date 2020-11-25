a2a221f27e3d34e332944d5be29b8096d0fcc1c0
EduardoWhite <47611990+EduardoWhite@users.noreply.github.com>
2020-11-25 03:41:46 -0800

    Create codigo.cpp

---
+++ codigo.cpp
@@ -0,0 +1,18 @@
+/*
+Blanco Melendez Manuel Eduardo
+Programa: Suma de dos numeros
+github: https://github.com/EduardoWhite
+*/
+
+
+#include <iostream>
+
+int main()
+{    
+     int a = 5;
+     int b = 5;
+     
+     int res =  a +  b;
+
+     std::cout << "Suma: " << res <<std::endl;
+}
